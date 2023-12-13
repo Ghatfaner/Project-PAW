49497d142528216e2714c3cb1e0a6420702d3dda
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-12-14 06:55:49 +0700

    [Add] adding navbar and finish the homepage

--- view/index.php
+++ view/index.php
@@ -22,14 +22,21 @@ if (!isset($_SESSION['userId'])) {
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="../css/styles.css">
-  <title>Document</title>
+  <title>Home</title>
 
   <style>
+    body {
+      overflow-x: hidden;
+    }
     .navbar {
       background-color: #005B41;
     }
     .navbar-brand img {
       width: 100%;
+    }
+    .footer {
+      background-color: #005B41;
+      color: #E6EFEC;
     }
     .container-fluid {
       display: flex;
@@ -40,7 +47,7 @@ if (!isset($_SESSION['userId'])) {
       margin-right: 12px;
     }
     .carousel-item {
-      height: 32rem;
+      height: 36rem;
       background: #232D3F;
       color: #e7e7e7;
       position: relative;
@@ -54,6 +61,11 @@ if (!isset($_SESSION['userId'])) {
       right: 0;
       padding-bottom: 3rem;
     }
+    .container a{
+      border: none;
+      background-color: #2B7761;
+      color: #E6EFEC;
+    }
     .overlay-image {
       position: absolute;
       top: 0;
@@ -64,10 +76,40 @@ if (!isset($_SESSION['userId'])) {
       background-size: cover;
       opacity: 0.5;
     }
+    .card-img {
+      max-height: 32rem;
+      opacity: 0.5;
+    }
+    .card-img-top {
+      height: 12rem;
+      object-fit: cover;
+    }
+    .container-card-big {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      border-radius: 0 0 0.25rem 0.25rem;
+      padding-bottom: 11rem;
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(35, 45, 63, 0));
+    }
+    .card {
+      background-color: #232D3F;
+      color: #e7e7e7;
+    }
+    .card a {
+      border: none;
+      background-color: #2B7761;
+      color: #E6EFEC;
+    }
+    .card a:hover {
+      background-color: #E6EFEC;
+      color: #2B7761;
+    }
   </style>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container-fluid px-4">
 
       <a class="navbar-brand" href="../view/index.php">
@@ -83,7 +125,7 @@ if (!isset($_SESSION['userId'])) {
             <a class="nav-link text-white-50" href="../view/search.php">Search</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white-50" href="../view/">Category</a>
+            <a class="nav-link text-white-50" href="../view/category.php">Category</a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-white-50" href="../view/bookmark.php">Bookmark</a>
@@ -105,31 +147,31 @@ if (!isset($_SESSION['userId'])) {
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <div class="overlay-image" style="background-image:url(../pictures/slide-1.jpg);"></div>
+        <div class="overlay-image" style="background-image:url(../pictures/movie-wide/1.png);"></div>
         <div class="container">
           <h1>Example headline</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
-          <a href="#" class="btn btn-lg btn-primary">
+          <a href="#" class="btn btn-lg ">
             Read more
           </a>
         </div>
       </div>
       <div class="carousel-item">
-        <div class="overlay-image" style="background-image:url(../pictures/slide-2.jpg);"></div>
+        <div class="overlay-image" style="background-image:url(../pictures/movie-wide/2.png);"></div>
         <div class="container">
           <h1>Example headline</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
-          <a href="#" class="btn btn-lg btn-primary">
+          <a href="#" class="btn btn-lg ">
             Read more
           </a>
         </div>
       </div>
       <div class="carousel-item">
-        <div class="overlay-image" style="background-image:url(../pictures/slide-3.jpg);"></div>
+        <div class="overlay-image" style="background-image:url(../pictures/movie-wide/3.png);"></div>
         <div class="container">
           <h1>Example headline</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
-          <a href="#" class="btn btn-lg btn-primary">
+          <a href="#" class="btn btn-lg ">
             Read more
           </a>
         </div>
@@ -145,10 +187,202 @@ if (!isset($_SESSION['userId'])) {
     </a>
   </div>
 
+  <div class="container-card mt-5 mb-5">
+    <div class="row px-5">
+      <h3 class="mb-3">Popular Actions</h3>
+      
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/14.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/17.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/47.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/45.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container-card mt-5 mb-5">
+    <div class="row px-5">
+      <h3 class="mb-3">Popular Documentaries</h3>
+      
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/7.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/35.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/8.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-3">
+        <div class="card border border-0">
+          <img src="../pictures/movie-wide/29.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="px-5">
+    <div class="card rounded">
+      <img src="../pictures/thumbnail/thumbnail-1.png" alt="" class="card-img">
+      <div class="container-card-big">
+        <div class="card-img-overlay px-5">
+          <h3 class="card-title">Card title</h3>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn px-3 py-2">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-card mt-5 mb-5">
+    <div class="row px-5">
+      <h3 class="mb-3">Upcoming Review Movies</h3>
+      
+      <div class="col">
+        <div class="card border border-0">
+          <img src="../pictures/up-movie-wide/up-1.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card border border-0">
+          <img src="../pictures/up-movie-wide/up-2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card border border-0">
+          <img src="../pictures/up-movie-wide/up-3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  
+  <div class="px-5"><a href="../controller/logout.php" class="btn btn-warning w-100 mb-5 fs-3 fw-semibold">Logout</a></div>
+
+  <div class="footer">
+    <div class="container-fluid px-5 py-5 d-flex flex-column">
+      <div class="pb-5">
+        <img src="../pictures/Logo-Light-Small.png" alt="">
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <h5 class="mb-3">Home</h5>
+          <p>Discover a variety of exciting content and explore the latest updates.</p>
+        </div>
+        <div class="col-3">
+          <h5 class="mb-3">Search</h5>
+          <p>See relevant information quickly and easily using our powerful search feature.</p>
+        </div>
+        <div class="col-3">
+          <h5 class="mb-3">Category</h5>
+          <p>Explore a wide range of categories and find content tailored to your interests.</p>
+        </div>
+        <div class="col-3">
+          <h5 class="mb-3">Social Media</h5>
+          <p>Connect with us on social media for the latest updates and engaging content.</p>
+          <div class="text-dark d-flex flex-row justify-content-start gap-2">
+            <div>
+              <i class="fab fa-facebook-square fa-3x"></i>
+            </div>
+            <div>
+              <i class="fab fa-whatsapp-square fa-3x"></i>
+            </div>
+            <div>
+              <i class="fab fa-twitter-square fa-3x"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-
-  <a href="../controller/logout.php" class="btn btn-warning">logout</a>
 
 </body>
 </html>
