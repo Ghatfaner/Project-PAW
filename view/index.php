25c6a6e7001c17296cacf310a761befa00c62e04
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-12-13 23:03:19 +0700

    [Add] adding view in index.php

--- view/index.php
+++ view/index.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
+
 if (!isset($_SESSION['userId'])) {
-  header("Location: login.php");
-  die();
+    header("Location: login.php");
+    die();
 }
 ?>
 
@@ -11,18 +12,143 @@ if (!isset($_SESSION['userId'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="../css/styles.css">
   <title>Document</title>
+
+  <style>
+    .navbar {
+      background-color: #005B41;
+    }
+    .navbar-brand img {
+      width: 100%;
+    }
+    .container-fluid {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .nav-item {
+      margin-right: 12px;
+    }
+    .carousel-item {
+      height: 32rem;
+      background: #232D3F;
+      color: #e7e7e7;
+      position: relative;
+      background-position: center;
+      background-size: cover;
+    }
+    .container {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      padding-bottom: 3rem;
+    }
+    .overlay-image {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-position: center;
+      background-size: cover;
+      opacity: 0.5;
+    }
+  </style>
 </head>
 <body>
-  <div class="container">
-    <h1>Welcome to homepage</h1>
-    <a href="../controller/logout.php" class="btn btn-warning">logout</a>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid px-4">
+
+      <a class="navbar-brand" href="../view/index.php">
+        <img src="../pictures/Logo-Light-Small.png" alt="">
+      </a>
+
+      <div class="d-flex justify-content-end" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="../view/index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white-50" href="../view/search.php">Search</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white-50" href="../view/">Category</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white-50" href="../view/bookmark.php">Bookmark</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white-50" href="../view/profile.php">Profile</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </nav>
+
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="overlay-image" style="background-image:url(../pictures/slide-1.jpg);"></div>
+        <div class="container">
+          <h1>Example headline</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
+          <a href="#" class="btn btn-lg btn-primary">
+            Read more
+          </a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="overlay-image" style="background-image:url(../pictures/slide-2.jpg);"></div>
+        <div class="container">
+          <h1>Example headline</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
+          <a href="#" class="btn btn-lg btn-primary">
+            Read more
+          </a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="overlay-image" style="background-image:url(../pictures/slide-3.jpg);"></div>
+        <div class="container">
+          <h1>Example headline</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis esse qui molestias beatae explicabo alias et tempore. Tempore vel distinctio reprehenderit est repellendus nesciunt impedit cumque! Debitis velit accusantium rem.</p>
+          <a href="#" class="btn btn-lg btn-primary">
+            Read more
+          </a>
+        </div>
+      </div>
+    </div>
+    <a href="#myCarousel" class="carousel-control-prev" role="button" data-bs-slide="prev">
+      <span class="sr-only">Previous</span>
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    </a>
+    <a href="#myCarousel" class="carousel-control-next" role="button" data-bs-slide="next">
+      <span class="sr-only">Next</span>
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    </a>
   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+  <a href="../controller/logout.php" class="btn btn-warning">logout</a>
+
 </body>
 </html>
--- view/registration.php
+++ view/registration.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
+
 if (isset($_SESSION['userId'])) {
-  header("Location: index.php");
-  die();
+    header("Location: index.php");
+    die();
 }
 ?>
 
@@ -16,17 +17,21 @@ if (isset($_SESSION['userId'])) {
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" href="../css/styles.css">
 
   <title>Registration</title>
   <style>
     .container {
-      width: 50%;
+      max-width: 50%;
     }
     .h3 {
       font-weight: 500;
     }
-    .form-label {
+    .mb-1 {
       margin-bottom: 4px
     }
     .container {
@@ -35,95 +40,94 @@ if (isset($_SESSION['userId'])) {
     }
     .submit-btn {
       border: none;
-      padding: 12px 12px;
-      background-color: #FDB827;
-      color: #232D3F;
+      background-color: #2B7761;
+      color: #E6EFEC;
       font-weight: 600;
     }
   </style>
 </head>
 <body>
     <div class="container mt-5 mb-5 rounded">
-      <h3 class="mb-3 d-flex justify-content-center">Sign-up</h3>
+      <p class="mb-5 d-flex justify-content-center fs-3 fw-semibold">Sign-up</p>
       <div class="d-flex justify-content-center">
         <img src="../pictures/Logo-Light-Big.png" alt="registration" class="mb-5">
       </div>
       <?php
-      if (isset($_POST['submit'])) {
-        $username = isset($_POST['username']) ? $_POST['username'] : '';
-        $email = isset($_POST['email']) ? $_POST['email'] : '';
-        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-        
-        // Check if the "occupation" key exists before accessing it
-        $occupation = isset($_POST['occupation']) ? $_POST['occupation'] : '';
-        
-        $address = isset($_POST['address']) ? $_POST['address'] : '';
-        $password = isset($_POST['password']) ? $_POST['password'] : '';
-        $passwordConfirm = isset($_POST['passwordConfirm']) ? $_POST['passwordConfirm'] : '';
+        if (isset($_POST['submit'])) {
+            $username = isset($_POST['username']) ? $_POST['username'] : '';
+            $email = isset($_POST['email']) ? $_POST['email'] : '';
+            $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
 
-        $errors = array();
+            // Check if the "occupation" key exists before accessing it
+            $occupation = isset($_POST['occupation']) ? $_POST['occupation'] : '';
 
-        if (strlen($password) < 8) {
-            array_push($errors, "The password must be at least 8 characters");
-        }
-        if ($password !== $passwordConfirm) {
-            array_push($errors, "The passwords do not match");
-        }
+            $address = isset($_POST['address']) ? $_POST['address'] : '';
+            $password = isset($_POST['password']) ? $_POST['password'] : '';
+            $passwordConfirm = isset($_POST['passwordConfirm']) ? $_POST['passwordConfirm'] : '';
 
-        if (count($errors) > 0) {
-            foreach ($errors as $error) {
-                echo "<div class='alert alert-danger' role='alert'>$error</div>";
+            $errors = array();
+
+            if (strlen($password) < 8) {
+                array_push($errors, "The password must be at least 8 characters");
             }
-        } else {
-            require_once "../connDB.php"; // Check the path here
-            require_once "../controller/control.php"; // Check the path here
-            $sql = new control(); // Make sure control class is defined
-            $result = $sql->c_signUp($username, $email, $password, $address, 
-            $phone, $occupation);
-            if ($result == 'success') {
-                echo "<div class='alert alert-success' role='alert'>Registration success. You can login <a style=\"color: green;\" href=\"../view/login.php\">here.</a></div>";
+            if ($password !== $passwordConfirm) {
+                array_push($errors, "The passwords do not match");
+            }
+
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger' role='alert'>$error</div>";
+                }
             } else {
-                echo "<div class='alert alert-danger' role='alert'>Registration failed, email already exist.</div>";
+                require_once "../connDB.php"; // Check the path here
+                require_once "../controller/control.php"; // Check the path here
+                $sql = new control(); // Make sure control class is defined
+                $result = $sql->c_signUp($username, $email, $password, $address,
+                    $phone, $occupation);
+                if ($result == 'success') {
+                    header("Location: confirmRegist.php");
+                } else {
+                    echo "<div class='alert alert-danger' role='alert'>Registration failed, email already exist.</div>";
+                }
             }
         }
-      }
       ?>
       <form action="registration.php" method="post">
         <div class="row mb-2">
           <div class="col">
-            <div class="form-label">Username</div>
+            <div class="mb-1">Username</div>
             <input type="text" name="username" class="form-control rounded" placeholder="Username" required>
           </div>
           <div class="col">
-            <div class="form-label">Email</div>
+            <div class="mb-1">Email</div>
             <input type="email" name="email" class="form-control rounded" placeholder="Email" required>
           </div>
         </div>
         <div class="row mb-2">
           <div class="col">
-            <div class="form-label">Phone Number</div>
+            <div class="mb-1">Phone Number</div>
             <input type="tel" name="phone" class="form-control rounded" placeholder="Phone number" pattern="[0-9]{4}-[0-9]{4}-[0-9]{4}" required>
           </div>
           <div class="col">
-            <div class="form-label">Occupation</div>
+            <div class="mb-1">Occupation</div>
             <input type="text" name="occupation" class="form-control rounded" placeholder="Occupation" required>
           </div>
         </div>
         <div class="mb-2">
-          <div class="form-label">Address</div>
+          <div class="mb-1">Address</div>
           <textarea name="address" class="form-control rounded" placeholder="Address" rows="2" cols="30" required></textarea>
         </div>
         <div class="mb-2">
-          <div class="form-label">Password</div>
+          <div class="mb-1">Password</div>
           <input type="password" name="password" class="form-control rounded" placeholder="Password" required>
         </div>
         <div class="mb-5">
-          <div class="form-label">Confirmation Password</div>
+          <div class="mb-1">Confirmation Password</div>
           <input type="password" name="passwordConfirm" class="form-control rounded" placeholder="Confirm password" required>
         </div>
-        <div>
-          <input type="submit" name="submit" class="form-control rounded submit-btn" value="Sign-up">
-        </div>
+        
+        <input type="submit" name="submit" class="form-control rounded submit-btn py-3 fs-5" value="Sign-up">
+        
       </form>
     </div>
 </body>
