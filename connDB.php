e2e20fb7a7a83601eb6afcb22a51341ea6c433fb
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-12-13 23:03:30 +0700

    [Add] adding new view for the user

--- connDB.php
+++ connDB.php
@@ -10,7 +10,7 @@ class connDB {
     $dbName = "paw";
     $conn = mysqli_connect($hostName, $dbUser, $dbPass, $dbName);
     // ganti user ma password sesuai database masing2
-    // p.s. milzam
+    // p.s. milzam & ghatfan
 
     if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
