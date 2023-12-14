ecbeb47b8c72c307aa032c93bfc35399be06c3be
Milzam Radifan <milzam_radifan@student.ub.ac.id>
2023-12-14 07:04:28 +0700

    [add] category page

--- view/category.php
+++ view/category.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Category</title>
-</head>
-<body>
-  <div>
-    <h4>Sort By:</h4>
-    <select name="" id="">
-      <option value="">Ascending (A-Z)</option>
-      <option value="">Descending (Z-A)</option>
-      <option value="">Date (Latest)</option>
-      <option value="">Date (Oldest)</option>
-      <option value="">Lower Price</option>
-      <option value="">Higher Price</option>
-      <option value="">Age Rating</option>
-    </select>
-  </div>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <div>
-    
-  </div>
-</body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Category</title>
+  </head>
+  <body>
+    <div class="dropdown">
+      
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Sort
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Ascending (A-Z)</a></li>
+        <li><a class="dropdown-item" href="#">Descending (Z-A)</a></li>
+        <li><a class="dropdown-item" href="#">Date (Latest)</a></li>
+        <li><a class="dropdown-item" href="#">Date (Oldest)</a></li>
+        <li><a class="dropdown-item" href="#">Lower Price</a></li>
+        <li><a class="dropdown-item" href="#">Higher Price</a></li>
+        <li><a class="dropdown-item" href="#">Age Rating</a></li>
+      </ul>
+    </div>
+
+    <div>
+
+    </div>
+  </body>
 </html>
